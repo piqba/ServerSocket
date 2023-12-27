380c7f2946333a78e22fc59292f640e68b2f59c8
Alexei <alexei.quiroga@wallit.eu>
2023-12-27 15:34:28 -0500

    removed namespace

--- src/ServerSocket/ServerSocket.cs
+++ src/ServerSocket/ServerSocket.cs
@@ -2,161 +2,157 @@
 using System.Net.Sockets;
 using System.Text;
 
-namespace ServerSocket
-{
-    public class ServerSocket(
+public class ServerSocket(
         int PortListening,
         int SocketBufferSize)
+{
+    private readonly int _portListening = PortListening;
+    private readonly int _socketBufferSize = SocketBufferSize;
+    /// <summary>
+    /// Event raised when data is received
+    /// </summary>
+    public event Action<string> DataReceived;
+    /// <summary>
+    /// Event raised when a client is disconnected, return the client IP address
+    /// </summary>
+    public event Action<string> DisconnectedClient;
+    /// <summary>
+    /// Event raised when a client is connected, return the client IP address
+    /// </summary>
+    public event Action<string> ConnectedClient;
+    /// <summary>
+    /// Encoding type for the socket, default is ASCII
+    /// </summary>
+    public DataEncodingType EncodingType { get; set; } = DataEncodingType.ASCII;
+    /// <summary>
+    /// Server IP address, its optional, default is any local IP address
+    /// </summary>
+    public string ServerIP { get; set; } = IPAddress.Loopback.ToString();
+
+    /// <summary>
+    /// Listen for incoming connections
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    /// <exception cref="Exception"></exception>
+    public async Task Listen(CancellationToken cancellationToken)
     {
-        private readonly int _portListening = PortListening;
-        private readonly int _socketBufferSize = SocketBufferSize;
-        /// <summary>
-        /// Event raised when data is received
-        /// </summary>
-        public event Action<string> DataReceived;
-        /// <summary>
-        /// Event raised when a client is disconnected, return the client IP address
-        /// </summary>
-        public event Action<string> DisconnectedClient;
-        /// <summary>
-        /// Event raised when a client is connected, return the client IP address
-        /// </summary>
-        public event Action<string> ConnectedClient;
-        /// <summary>
-        /// Encoding type for the socket, default is ASCII
-        /// </summary>
-        public EncodingType EncodingType { get; set; } = EncodingType.ASCII;
-        /// <summary>
-        /// Server IP address, its optional, default is any local IP address
-        /// </summary>
-        public string ServerIP { get; set; } = IPAddress.Loopback.ToString();
+        var localAddress = CheckAndReturnServerAddress();
+        // Create socket
+        var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        socket.Bind(new IPEndPoint(localAddress, _portListening));
+        socket.Listen(0);
 
-        /// <summary>
-        /// Listen for incoming connections
-        /// </summary>
-        /// <param name="cancellationToken"></param>
-        /// <returns></returns>
-        /// <exception cref="Exception"></exception>
-        public async Task Listen(CancellationToken cancellationToken)
+        // Accept connections
+        while (!cancellationToken.IsCancellationRequested)
         {
-            var localAddress = CheckAndReturnServerAddress();
-            // Create socket
-            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            socket.Bind(new IPEndPoint(localAddress, _portListening));
-            socket.Listen(0);
+            Socket clientSocket = await socket.AcceptAsync(cancellationToken);
+            string clientIP = clientSocket.RemoteEndPoint!.ToString()!;
+            ConnectedClient?.Invoke(clientIP);
+            _ = Task.Run(async () => await HandleClient(clientSocket, cancellationToken), cancellationToken);
+        }
+    }
 
-            // Accept connections
+    #region Private Methods
+    private IPAddress CheckAndReturnServerAddress()
+    {
+        if (!IsPortAvailable(_portListening))
+            throw new Exception($"Port {_portListening} is not available");
+
+        IPAddress? propertyAddress;
+        try
+        {
+            propertyAddress = IPAddress.Parse(ServerIP);
+        }
+        catch (FormatException)
+        {
+            throw new Exception($"Server ip {ServerIP} not in the correct format."); ;
+        }
+
+        // Get local ip address
+        return IPAddress.IsLoopback(propertyAddress) ? GetLocalIP()
+            ?? throw new Exception("Local IP address not found")
+            : propertyAddress;
+    }
+    private static bool IsPortAvailable(int port)
+    {
+        using var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        try
+        {
+            socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            socket.Bind(new IPEndPoint(IPAddress.Any, port));
+            return true;
+        }
+        catch (SocketException)
+        {
+            return false;
+        }
+    }
+    private async Task HandleClient(Socket clientSocket, CancellationToken cancellationToken)
+    {
+        try
+        {
+            byte[] buffer = new byte[_socketBufferSize];
             while (!cancellationToken.IsCancellationRequested)
             {
-                Socket clientSocket = await socket.AcceptAsync(cancellationToken);
-                string clientIP = clientSocket.RemoteEndPoint!.ToString()!;
-                ConnectedClient?.Invoke(clientIP);
-                _ = Task.Run(async () => await HandleClient(clientSocket, cancellationToken), cancellationToken);
-            }
-        }
-
-        #region Private Methods
-        private IPAddress CheckAndReturnServerAddress()
-        {
-            if (!IsPortAvailable(_portListening))
-                throw new Exception($"Port {_portListening} is not available");
-
-            IPAddress? propertyAddress;
-            try
-            {
-                propertyAddress = IPAddress.Parse(ServerIP);
-            }
-            catch (FormatException)
-            {
-                throw new Exception($"Server ip {ServerIP} not in the correct format."); ;
-            }
-
-            // Get local ip address
-            return IPAddress.IsLoopback(propertyAddress) ? GetLocalIP()
-                ?? throw new Exception("Local IP address not found")
-                : propertyAddress;
-        }
-        private static bool IsPortAvailable(int port)
-        {
-            using var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            try
-            {
-                socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
-                socket.Bind(new IPEndPoint(IPAddress.Any, port));
-                return true;
-            }
-            catch (SocketException)
-            {
-                return false;
-            }
-        }
-        private async Task HandleClient(Socket clientSocket, CancellationToken cancellationToken)
-        {
-            try
-            {
-                byte[] buffer = new byte[_socketBufferSize];
-                while (!cancellationToken.IsCancellationRequested)
+                try
                 {
-                    try
-                    {
-                        int bytesRead = await clientSocket.ReceiveAsync(buffer);
+                    int bytesRead = await clientSocket.ReceiveAsync(buffer);
 
-                        if (bytesRead == 0)
-                        {
-                            DisconnectedClient?.Invoke(clientSocket.RemoteEndPoint!.ToString()!);
-                            clientSocket.Close();
-                            clientSocket.Dispose();
-                            break;
-                        }
-
-                        var clientData = DecodeMessage(buffer, bytesRead, EncodingType);
-                        DataReceived?.Invoke(clientData);
-                    }
-                    catch (SocketException)
+                    if (bytesRead == 0)
                     {
                         DisconnectedClient?.Invoke(clientSocket.RemoteEndPoint!.ToString()!);
+                        clientSocket.Close();
+                        clientSocket.Dispose();
+                        break;
                     }
 
+                    var clientData = DecodeMessage(buffer, bytesRead, EncodingType);
+                    DataReceived?.Invoke(clientData);
+                }
+                catch (SocketException)
+                {
+                    DisconnectedClient?.Invoke(clientSocket.RemoteEndPoint!.ToString()!);
                 }
 
             }
-            catch (Exception ex)
-            {
-                // Handle the exception here
-                Console.WriteLine($"An error occurred while handling the client: {ex.Message}");
-            }
-        }
-        private static IPAddress? GetLocalIP()
-        {
-            // Obtain all local ip addresses
-            IPAddress[] localAddresses = Dns.GetHostAddresses(Dns.GetHostName());
-            IPAddress? localAddress = null;
-            if (localAddresses is not null)
-            {
-                localAddress = localAddresses
-                    .Where(address => address.AddressFamily == AddressFamily.InterNetwork) // Only IPv4
-                    .Where(address => address.GetAddressBytes()[0] != 169) // Exclude link-local addresses (169.254.*.*
-                    .Where(address => address.GetAddressBytes()[0] != 10) // 
-                    .FirstOrDefault(address => !IPAddress.IsLoopback(address)); // Exclude loopback
-            }
-            return localAddress;
-        }
-        private static string DecodeMessage(byte[] buffer, int bytesRead, EncodingType encodingType = EncodingType.ASCII)
-        {
-            string clientData = encodingType switch
-            {
-                EncodingType.ASCII => Encoding.ASCII.GetString(buffer, 0, bytesRead),
-                EncodingType.UTF8 => Encoding.UTF8.GetString(buffer, 0, bytesRead),
-                EncodingType.UTF32 => Encoding.UTF32.GetString(buffer, 0, bytesRead),
-                EncodingType.Unicode => Encoding.Unicode.GetString(buffer, 0, bytesRead),
-                _ => throw new Exception("Encoding type not supported")
-            };
-            return clientData;
-        }
-        #endregion
-    }
 
-    public enum EncodingType
+        }
+        catch (Exception ex)
+        {
+            // Handle the exception here
+            Console.WriteLine($"An error occurred while handling the client: {ex.Message}");
+        }
+    }
+    private static IPAddress? GetLocalIP()
+    {
+        // Obtain all local ip addresses
+        IPAddress[] localAddresses = Dns.GetHostAddresses(Dns.GetHostName());
+        IPAddress? localAddress = null;
+        if (localAddresses is not null)
+        {
+            localAddress = localAddresses
+                .Where(address => address.AddressFamily == AddressFamily.InterNetwork) // Only IPv4
+                .Where(address => address.GetAddressBytes()[0] != 169) // Exclude link-local addresses (169.254.*.*
+                .Where(address => address.GetAddressBytes()[0] != 10) // 
+                .FirstOrDefault(address => !IPAddress.IsLoopback(address)); // Exclude loopback
+        }
+        return localAddress;
+    }
+    private static string DecodeMessage(byte[] buffer, int bytesRead, DataEncodingType encodingType = DataEncodingType.ASCII)
+    {
+        string clientData = encodingType switch
+        {
+            DataEncodingType.ASCII => Encoding.ASCII.GetString(buffer, 0, bytesRead),
+            DataEncodingType.UTF8 => Encoding.UTF8.GetString(buffer, 0, bytesRead),
+            DataEncodingType.UTF32 => Encoding.UTF32.GetString(buffer, 0, bytesRead),
+            DataEncodingType.Unicode => Encoding.Unicode.GetString(buffer, 0, bytesRead),
+            _ => throw new Exception("Encoding type not supported")
+        };
+        return clientData;
+    }
+    #endregion
+    public enum DataEncodingType
     {
         ASCII,
         UTF8,
